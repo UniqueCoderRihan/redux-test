c4b38f1a76e50214894cc399da723feffaa2116c
Rihanul Islam <rihanthedev@gmail.com>
2023-08-31 19:29:06 +0600

    inintal setup redux part 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h2 className="bg-purple-400 text-2xl">Redux </h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,10 @@
+
+const Counter = () => {
+    return (
+        <div>
+            <h1>This is Counter spp</h1>
+        </div>
+    );
+};
+
+export default Counter;
